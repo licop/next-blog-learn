6a2253beba298467b6132addf568644b81d3ea90
licop <licop@users.noreply.github.com>
2020-11-05 18:26:59 +0800

    update

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'licop1'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'licop';
+export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
   return (
